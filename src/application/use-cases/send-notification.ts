c696f589b3271f56089a3ee2bcc5a178a73e9dec
César Augusto <cesaraugusto.ramos@inatel.br>
2022-12-20 06:23:44 -0300

    Create notification use case and unit tests

---
+++ src/application/use-cases/send-notification.ts
@@ -0,0 +1,37 @@
+import { Content } from '../entities/content';
+import { Notification } from '../entities/Notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(
+    private readonly notificationsRepository: NotificationsRepository,
+  ) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, content, category } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content: new Content(content),
+      category,
+    });
+
+    await this.notificationsRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
