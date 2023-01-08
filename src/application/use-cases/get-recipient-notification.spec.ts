9b9e374d01c26fdb3559a275e345c49cbfad9561
César Augusto Ramos <cesaraugusto.ramos@inatel.br>
2023-01-08 15:44:00 -0300

    Implement get recipient notifications use case

---
+++ src/application/use-cases/get-recipient-notification.spec.ts
@@ -0,0 +1,36 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { GetRecipientNotification } from './get-recipient-notification';
+
+describe('get recipients notifications', () => {
+  it('should be able get recipient notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new GetRecipientNotification(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    const { notifications } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -4,6 +4,11 @@ import { NotificationsRepository } from '@application/repositories/notifications
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter(
       (notification) => notification.recipientId === recipientId,
